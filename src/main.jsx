c0d9add2a987176993ae4d5da898f3ca26a052f4
RamaiyaA <ramaiya.a@cisinlabs.com>
2024-04-08 18:59:59 +0530

    fixing bugs

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import 'slicknav/dist/jquery.slicknav.js'
+import 'jquery-nice-select/js/jquery.nice-select.js'
+import 'jquery-countdown/dist/jquery.countdown.js'
+import 'jquery.scrollbar/jquery.scrollbar.js'
+import 'magnific-popup/dist/jquery.magnific-popup.js'
+import 'owl.carousel/dist/owl.carousel.js'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'jquery/dist/jquery.min.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
